3ba49d4279745b9327c5b393d52f8cb99d74364f
vt-vu <vt-vu@gmofh.com>
2021-01-25 19:05:34 +0900

    Main logic

--- go.mod
+++ go.mod
@@ -2,3 +2,8 @@ module github.com/chunvv/ogp-parser
 
 go 1.15
 
+require (
+	github.com/PuerkitoBio/goquery v1.6.1
+	github.com/julianshen/go-readability v0.0.0-20160929030430-accf5123e283
+	github.com/stretchr/testify v1.7.0
+)
--- ogp.go
+++ ogp.go
@@ -1,5 +1,22 @@
 package ogpparser
 
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"net/http"
+	"reflect"
+
+	"strings"
+
+	"strconv"
+
+	"io/ioutil"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/julianshen/go-readability"
+)
+
 // Image content
 type OgImage struct {
 	Url    string `meta:"og:image,og:image:url"`
@@ -37,4 +54,197 @@ type PageInfo struct {
 	Videos      []*OgVideo
 	Audios      []*OgAudio
 	Content     string
+}
+
+func GetPageDataFromHtml(html []byte, data interface{}) error {
+	buf := bytes.NewBuffer(html)
+	doc, err := goquery.NewDocumentFromReader(buf)
+
+	if err != nil {
+		return err
+	}
+
+	return GetPageData(doc, data)
+}
+
+func GetPageData(doc *goquery.Document, data interface{}) error {
+	doc = goquery.CloneDocument(doc)
+	return getPageData(doc, data)
+}
+
+func GetPageInfoFromResponse(response *http.Response) (*PageInfo, error) {
+	info := PageInfo{}
+	html, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = GetPageDataFromHtml(html, &info)
+
+	if err != nil {
+		return nil, err
+	}
+
+	r, err := readability.NewDocument(string(html))
+	if err != nil {
+		return nil, err
+	}
+
+	info.Content = r.Text()
+
+	return &info, nil
+}
+
+func GetPageInfoFromUrl(urlStr string) (*PageInfo, error) {
+	resp, err := http.Get(urlStr)
+
+	if err != nil {
+		return nil, err
+	}
+	return GetPageInfoFromResponse(resp)
+}
+
+func getPageData(doc *goquery.Document, data interface{}) error {
+	var rv reflect.Value
+	var ok bool
+	if rv, ok = data.(reflect.Value); !ok {
+		rv = reflect.ValueOf(data)
+	}
+
+	if rv.Kind() != reflect.Ptr || rv.IsNil() {
+		return errors.New("should not be non-ptr or nil")
+	}
+
+	rt := rv.Type()
+
+	for i := 0; i < rv.Elem().NumField(); i++ {
+		fv := rv.Elem().Field(i)
+		field := rt.Elem().Field(i)
+
+		switch fv.Type().Kind() {
+		case reflect.Ptr:
+			if fv.IsNil() {
+				fv.Set(reflect.New(fv.Type().Elem()))
+			}
+			e := getPageData(doc, fv)
+
+			if e != nil {
+				return e
+			}
+		case reflect.Struct:
+			e := getPageData(doc, fv.Addr())
+
+			if e != nil {
+				return e
+			}
+		case reflect.Slice:
+			if fv.IsNil() {
+				fv.Set(reflect.MakeSlice(fv.Type(), 0, 0))
+			}
+
+			switch field.Type.Elem().Kind() {
+			case reflect.Struct:
+				last := reflect.New(field.Type.Elem())
+				for {
+					data := reflect.New(field.Type.Elem())
+					e := getPageData(doc, data.Interface())
+
+					if e != nil {
+						return e
+					}
+
+					//Ugly solution (I can't remove nodes. Why?)
+					if !reflect.DeepEqual(last.Elem().Interface(), data.Elem().Interface()) {
+						fv.Set(reflect.Append(fv, data.Elem()))
+						last.Elem().Set(data.Elem())
+
+					} else {
+						break
+					}
+				}
+			case reflect.Ptr:
+				last := reflect.New(field.Type.Elem().Elem())
+				for {
+					data := reflect.New(field.Type.Elem().Elem())
+					e := getPageData(doc, data.Interface())
+
+					if e != nil {
+						return e
+					}
+
+					//Ugly solution (I can't remove nodes. Why?)
+					if !reflect.DeepEqual(last.Elem().Interface(), data.Elem().Interface()) {
+						fv.Set(reflect.Append(fv, data))
+						last.Elem().Set(data.Elem())
+
+					} else {
+						break
+					}
+				}
+			default:
+				if tag, ok := field.Tag.Lookup("meta"); ok {
+					tags := strings.Split(tag, ",")
+
+					for _, t := range tags {
+						var contents []reflect.Value
+
+						processMeta := func(idx int, sel *goquery.Selection) {
+							if c, existed := sel.Attr("content"); existed {
+								if field.Type.Elem().Kind() == reflect.String {
+									contents = append(contents, reflect.ValueOf(c))
+								} else {
+									i, e := strconv.Atoi(c)
+
+									if e == nil {
+										contents = append(contents, reflect.ValueOf(i))
+									}
+								}
+
+								fv.Set(reflect.Append(fv, contents...))
+							}
+						}
+
+						doc.Find(fmt.Sprintf("meta[property=\"%s\"]", t)).Each(processMeta)
+
+						doc.Find(fmt.Sprintf("meta[name=\"%s\"]", t)).Each(processMeta)
+
+						fv = reflect.Append(fv, contents...)
+					}
+				}
+			}
+		default:
+			if tag, ok := field.Tag.Lookup("meta"); ok {
+
+				tags := strings.Split(tag, ",")
+
+				content := ""
+				existed := false
+				sel := (*goquery.Selection)(nil)
+				for _, t := range tags {
+					if sel = doc.Find(fmt.Sprintf("meta[property=\"%s\"]", t)).First(); sel.Size() > 0 {
+						content, existed = sel.Attr("content")
+					}
+
+					if !existed {
+						if sel = doc.Find(fmt.Sprintf("meta[name=\"%s\"]", t)).First(); sel.Size() > 0 {
+							content, existed = sel.Attr("content")
+						}
+					}
+
+					if existed {
+						if fv.Type().Kind() == reflect.String {
+							fv.Set(reflect.ValueOf(content))
+						} else if fv.Type().Kind() == reflect.Int {
+							if i, e := strconv.Atoi(content); e == nil {
+								fv.Set(reflect.ValueOf(i))
+							}
+						}
+						break
+					}
+				}
+			}
+		}
+	}
+	return nil
 }
